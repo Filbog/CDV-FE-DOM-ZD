368a3098bc27942f48dd19e398b23cffd9ddb0fd
Filbog <f.boguslawski101@gmail.com>
2024-02-22 16:49:09 +0100

    basic logic seems to work

--- tic-tac-toe/js/index.js
+++ tic-tac-toe/js/index.js
@@ -1,36 +1,48 @@
-// Game start
-// X is the current player
-// After each click on the board, current_player changes
-
-// JavaScript
-let currentPlayer = "X";
-let gameBoard = [
-  ["", "", ""],
-  ["", "", ""],
-  ["", "", ""],
-];
+//////// set up ///////
+let currentPlayer;
+let gameBoard;
 
 let cells = document.getElementsByClassName("cell");
+let result = document.getElementById("result");
+let playAgainBtn = document.getElementById("play-again");
+playAgainBtn.addEventListener("click", function () {
+  startNewGame();
+});
 
+//adding event listener to every cell in our 3x3 grid
 for (let cell of cells) {
   cell.addEventListener("click", function () {
     let id = cell.id;
     let row = parseInt(id[0]);
     let col = parseInt(id[1]);
-    console.log(id, row, col);
     cellClicked(row, col);
   });
 }
 
+startNewGame();
+
+function startNewGame() {
+  currentPlayer = "X";
+  gameBoard = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  for (let cell of cells) {
+    cell.innerText = "";
+  }
+  playAgainBtn.style.display = "none";
+  result.innerText = "";
+}
+
 function cellClicked(row, col) {
   if (gameBoard[row][col] == "") {
+    //populate the game board 2d array and the HTML element
     gameBoard[row][col] = currentPlayer;
     document.getElementById(`${row}${col}`).innerHTML =
       cellContent(currentPlayer);
-    console.log(currentPlayer);
-    console.log(gameBoard);
-    // check_winning_conditions();
-    check_for_tie();
+    //check if the game finished; if not, next player has their move
+    gameFinishConditions();
     currentPlayer = currentPlayer == "X" ? "O" : "X";
   }
 }
@@ -39,7 +51,19 @@ function cellContent(symbol) {
   return `<h1 class='display-1'>${symbol}</h1>`;
 }
 
-function check_winning_conditions(currentPlayer, gameBoard) {
+//////// checking if (and how) game finished ///////
+
+function gameFinishConditions() {
+  if (playerWon(currentPlayer)) {
+    result.innerText = `${currentPlayer} won, congrats!`;
+    playAgainBtn.style.display = "inline-block";
+  } else if (tie()) {
+    result.innerText = `It's a tie!`;
+    playAgainBtn.style.display = "inline-block";
+  }
+}
+
+function playerWon(currentPlayer) {
   // check for 3 in a row
   for (let row of gameBoard) {
     if (
@@ -75,7 +99,7 @@ function check_winning_conditions(currentPlayer, gameBoard) {
   // check for 3 in right-top-left-bottom diagonal
   topRightLeftDiagonal = true;
   for (let i = 2; i >= 0; i--) {
-    if (gameBoard[i][i] !== currentPlayer) {
+    if (gameBoard[i][2 - i] !== currentPlayer) {
       topRightLeftDiagonal = false;
       break;
     }
@@ -84,41 +108,15 @@ function check_winning_conditions(currentPlayer, gameBoard) {
     return true;
   }
 
-  // TO PRZYWROC
+  //if no win condition was met
   return false;
 }
 
-// tests
-let testGameBoard = [
-  ["O", "O", "X"],
-  ["X", "O", "O"],
-  ["O", "O", "X"],
-];
-
-console.log(check_winning_conditions("O", testGameBoard));
-
-function check_for_tie() {
+function tie() {
+  //check if every cell is populated
   return gameBoard.every(function (row) {
     return row.every(function (cell) {
       return cell !== "";
     });
   });
-}
-
-function correct_column() {
-  // Correct, working
-  for (let i = 0; i < gameBoard.length; i++) {
-    let temporaryColumn = [];
-    for (let row of gameBoard) {
-      temporaryColumn.push(row[i]);
-    }
-    console.log(temporaryColumn);
-    if (
-      temporaryColumn.every(function (cell) {
-        return cell === currentPlayer;
-      })
-    ) {
-      return true;
-    }
-  }
 }
